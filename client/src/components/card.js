ab9a8ee151237f8ad6feccf771393e2d691091d3
Nikhil Kavuri <nikhilkavuri1503@gmail.com>
2023-05-24 16:41:03 +0530

    Server part

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,37 +1,64 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Input from "@mui/joy/Input";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
 
 function MainCard() {
-    const [urlValue,setUrlValue] = useState("");
-    // Send the urlValue to the Api service
-    console.log(urlValue)
+  const [urlValue, setUrlValue] = useState("");
+  const API_URL = "http://localhost:5000";
+  const handleClick = () => {
+    fetch(API_URL + "/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        inputUrl: urlValue
+      })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      // Handle the response data here
+      console.log("Response from backend:", data.Shortened);
+      // Additional logic to process the response
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+  };
+  console.log(urlValue);
   return (
     <Card>
       <Card.Body>
-        <Card.Title style={{"position":"relative", "left":"40%"}}>Paste Your URL here</Card.Title>
+        <Card.Title style={{ position: "relative", left: "40%" }}>
+          Paste Your URL here
+        </Card.Title>
         <Card.Text>
-          <form
+        <form
             onSubmit={(event) => {
               event.preventDefault();
+              handleClick()
             }}
           >
-            <Input
-              placeholder="Your URL"
-              required
-              sx={{ mb: 1, fontSize: "var(--joy-fontSize-sm)" }}
-              endDecorator={<SearchIcon />}
-              value={urlValue}
-              onChange={(e)=>{
-                setUrlValue(e.target.value)
-              }}
-            />
-            <Button style={{"position":"relative", "left":"39%",width:"15em"}} type="submit">Search</Button>
+          <Input
+            placeholder="Your URL"
+            required
+            sx={{ mb: 1, fontSize: "var(--joy-fontSize-sm)" }}
+            endDecorator={<SearchIcon />}
+            value={urlValue}
+            onChange={(e) => {
+              setUrlValue(e.target.value);
+            }}
+          />
+          <Button
+            style={{ position: "relative", left: "39%", width: "15em" }}
+            type="submit"
+          >
+            Search
+          </Button>
           </form>
         </Card.Text>
-
       </Card.Body>
     </Card>
   );
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import db from "./config/db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+db.once("open", () => {
+  console.log("Connected to database");
+});
+db.on("error", (err) => {
+  console.log("Database error:", err);
+});
+app.get("/",(req,res)=>{
+    res.send('Connection success');
+})
+let url;
+app.post("/add",(req,res)=>{
+    console.log(req.body)
+    url = `https://google.com`
+    const randomKey = Math.floor(Math.random() * 100000); // Generate a random key
+    const shortUrl = `https://hasShort/${randomKey}`; 
+    res.send({ "Shortened": shortUrl });
+})
+app.get("/:key", (req, res) => {
+    const key = req.params.key;
+    if (key === "12334") { // Check if the provided key matches the expected value
+      res.redirect("https://google.com"); // Redirect to the main URL (https://google.com)
+    } else {
+      res.status(404).send("Shortened URL not found"); // Handle if the provided key doesn't match
+    }
+  });
+app.listen(5000, () => {
+  console.log(`server started on http://localhost:${5000}`);
+});
